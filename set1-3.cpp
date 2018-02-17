d032a87ae752b8f3343ad798c687b73d1953ee54
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-17 20:49:06 +0530

    Create set1-3.cpp

---
+++ set1-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+char n;
+	cout<<"Enter The Char:";
+	cin>>n;
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+	{
+		cout<<"\n"<<n<<" Is Vowels.";
+		
+	}
+	else if(n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+	{
+		cout<<"\n"<<n<<" Is Vowels.";
+		
+	}
+	else
+	{
+		cout<<"\n"<<n<<" Is Const.";
+}
+   return 0;
+}
+	
