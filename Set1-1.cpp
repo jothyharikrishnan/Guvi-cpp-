703ddb8390be1b7dc586d408f5f14931f6e80003
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-17 16:22:40 +0530

    Create Set1-1.cpp

---
+++ Set1-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() 
+{
+	// your code goes here
+	
+	int n;
+	cin>>n;
+	if(n<0)
+	{
+	cout<<"\nnegative:";
+	}
+	else if(n==0)
+	{
+		cout<<"\nZero";
+	}
+	else
+	{
+	    cout<<"\npositive";
+	}
+	
+	return 0;
+
+}
