9a36cf28cf9352637f8be41e4c1b54e4c20c2964
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-17 16:31:11 +0530

    Create set1-2.cpp

---
+++ set1-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  
+  cout<<"\nEnter The Num:";
+  cin>>n;
+  if(n%2==0)
+  {
+    cout<<"\n"<<n<<"is even num.";
+  }
+  else{
+    cout<<"\n"<<n<<" Is Odd Num. ";
+  }
+  return 0;
+}
