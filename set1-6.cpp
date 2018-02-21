1e537eb363d2fc2f82fadfef13f54954c84e225e
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-21 17:54:57 +0530

    Create set1-6.cpp

---
+++ set1-6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"Enter the num:";
+cin>>a;
+if(a%4==0)
+{
+    cout<<"\nLeapYear";
+}
+else
+{
+    cout<<"\nNot A LeapYear";
+}
+
+}
