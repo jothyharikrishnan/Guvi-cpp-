33f980de17db465ed23d497891a621ab234c3a6a
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-22 17:09:44 +0530

    Create set1-7.cpp

---
+++ set1-7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    
+    cout<<"Enter The num:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    cout<<"\nHello";
+
+    return 0;
+
+    
+}
