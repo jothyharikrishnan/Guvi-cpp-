db35fff3b339a674329ffe512bc5297d35f61f63
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-22 17:15:31 +0530

    Create set1-8.cpp

---
+++ set1-8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,sum=0;
+    
+    cout<<"Enter The num:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        
+    sum+=i;
+    
+}
+cout<<"\nSum Of the natural Num:"<<sum;
+    return 0;
+
+    
+}
+
+
+
