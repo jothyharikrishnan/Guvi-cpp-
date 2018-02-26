c960acc9b2e61ee9bba7e529f28558d8e396f1db
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-26 18:04:13 +0530

    Create set1-9.cpp

---
+++ set1-9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,k,i,j;
+    int sum=0;
+    cout<<"\nENter The N and K:";
+    cin>>n>>k;
+    int m;
+    for(i=1;i<=n;i++)
+    {
+        cin>>m;
+     
+    
+    }
+    for(i=1;i<=k;i++)
+        {
+            sum+=i;
+        }
+    
+     
+    cout<<sum;
+
+    return 0;
+}
+
