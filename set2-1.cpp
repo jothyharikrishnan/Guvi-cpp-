377fa140056acb64bc47d331cfb8cdc84319dea9
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-28 10:15:28 +0530

    Create set2-1.cpp

---
+++ set2-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+ int n,m,res;
+ cout<<"Enter The n and m:";
+ cin>>n>>m;
+ res=pow(n,m);
+ cout<<"\n"<<n<<" power of "<<m<<" is: "<<res;
+    return 0;
+}
