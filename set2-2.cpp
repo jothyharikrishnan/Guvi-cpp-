298080ca09799410f5169bb7415b06311c0871f9
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-02-28 10:35:44 +0530

    Create set2-2.cpp

---
+++ set2-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int n, num, digit, rev = 0;
+
+     cout << "Enter a number: ";
+     cin >> num;
+
+     n = num;
+
+      while (num != 0)
+      {
+         digit = num % 10;
+         rev = (rev * 10) + digit;
+         num = num / 10;
+     
+}
+     if (n == rev)
+         cout << " Yes";
+     else
+         cout << " No";
+
+    return 0;
+}
