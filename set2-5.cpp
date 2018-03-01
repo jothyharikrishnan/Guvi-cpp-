661717077f84d6b1cd141c0c29c3b8b853768177
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-01 13:39:33 +0530

    Create set2-5.cpp

---
+++ set2-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  int n,m;
+  cout<<"Enter The n and m:"<<endl;
+  cin>>n>>m;
+  for(int i=n;i<m;i++)
+  {
+            if(i%2==0)
+      {
+          cout<<i;
+          
+      }
+  }
+ return 0;
+}
