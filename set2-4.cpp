bac0adb9e615ccf4dc72283951498749f51f72c0
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-01 06:54:35 +0530

    Create set2-4.cpp

---
+++ set2-4.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  int n,m;
+  cout<<"Enter The n and m:"<<endl;
+  cin>>n>>m;
+  for(int i=n;i<m;i++)
+  {
+            if(i%2==1)
+      {
+          cout<<i;
+          
+      }
+  }
+ return 0;
+}
