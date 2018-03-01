ab12bacc1ef807e167edbebdd3a89a6a09673c70
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-01 20:38:52 +0530

    Create set2-10.cpp

---
+++ set2-10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,mul;
+    cout<<"Enter:"<<endl;
+    cin>>n;
+  
+    for(i=1;i<=5;i++)
+    
+    {
+        mul=n*i;
+    
+        cout<<mul<<"\t";
+    }
+    
+}
