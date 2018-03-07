04e59b7f742f6aea388fb35b2b4eb27c4b6b5a09
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-07 17:16:46 +0530

    34 - Count the number of lines in a given paragraph

---
+++ set4/set4-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout<<"Enter The String:";
+    getline(cin,s);
+    int count=0,i;
+    for(i=0;s[i]!=0;i++)
+    {
+        if(s[i]=='.')
+        {
+           count++; 
+        }
+  
+}
+cout<<count+1;
+    return 0;
+}
