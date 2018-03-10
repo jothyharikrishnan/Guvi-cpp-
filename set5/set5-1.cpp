763636440c1e256753aa0e3d93150f927a44e56c
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-10 22:17:43 +0530

    Update set5-1.cpp

--- set5/set5-1.cpp
+++ set5/set5-1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int k;
+    int i;
+    cout<<"Enter The String:"<<endl;
+    getline(cin,s);
+    cout<<"Enter The Num To Print the string K times:";
+    cin>>k;
+    for(i=1;i<=k;i++)
+    cout<<"\n"<<i<<"."<<s;
+
+    return 0;
+}
 
