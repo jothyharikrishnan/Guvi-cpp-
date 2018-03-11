1847f14edce2b01d450590c2448ad4e31afa05a7
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-11 23:36:38 +0530

    43 - Write a program to concatenate two strings without using strcat() function.

---
+++ set5/set5-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1;
+    string s2;
+    string res;
+    cout<<"Enter The String1:"<<endl;
+    cin>>s1;
+    cout<<"Enter The String1:"<<endl;
+    cin>>s2;
+    res=s1+s2;
+    cout<<"Now The String Is:"<<res;
+    
+
+    return 0;
+}
