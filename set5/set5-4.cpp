183a55ab4d822af2ea73a69c112d1276cc517eec
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-11 23:45:30 +0530

    44 - Given a number N check whether it is in the range [1,10](inclusive)

---
+++ set5/set5-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a num from range 1 to 10:";
+    cin>>n;
+    if(n>=1&&n<=10)
+    cout<<"Yes";
+    else
+    cout<<"No";
+    return 0;
+}
