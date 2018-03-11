4b9855a875ed24e2c499577a8e3b856fed98aa61
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-12 00:43:17 +0530

    49 - Given 2 numbers find their sum

---
+++ set5/set5-9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,sum;
+    cout<<"Enter 2 number:";
+    cin>>n>>m;
+        sum=n+m;
+    cout<<"Sum is: "<<sum;
+    return 0;
+}
