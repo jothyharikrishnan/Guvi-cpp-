653800659860542570626b1df86e85f8e90066bf
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-12 21:29:33 +0530

    53 - Given a number N, Print the sum of all its digits.

---
+++ set6/set6-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,a[20],temp,i,sum=0;
+    cout<<"Enter a number from (1-10)";
+    cin>>n;
+    i=0;
+    while(n>0)
+    {
+    temp=n%10;
+    a[i]=temp;
+    n/=10;
+    sum+=a[i];
+    }
+    cout<<sum;
+    return 0;
+}
