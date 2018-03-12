c8b3f8187b33ab782f780697a3598a7f9ccc1d3d
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-13 00:09:31 +0530

    54 - Given a number N, Print the nearest even number which is less than or equal to N.

---
+++ set6/set6-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    if(n%2==0)
+    cout<<n;
+    else
+    cout<<n-1;
+    return 0;
+}
