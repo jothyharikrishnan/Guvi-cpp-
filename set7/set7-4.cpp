dd4d31cee345dea0a811ed02f1a9b587958b12e3
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-13 23:30:18 +0530

    64 - Given 2 numbers N and M add it and check whether the result is odd or even

---
+++ set7/set7-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,sum;
+    cout<<"Enter 2 Num:";
+    cin>>n>>m;
+    sum=n+m;
+    if(sum%2==0)
+    cout<<"Even";
+    else
+    cout<<"odd";
+}
