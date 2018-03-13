09cd0a3a91868ab76ab60c06c0fa5e244e649b61
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-13 21:18:42 +0530

    60 - Given a number N find print fibonacci series of N numbers

--- set6/set6-10.cpp
+++ set6/set6-10.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    int a,i,f1,f2,f3;
+    int a,j,f1,f2,f3;
     f1=f2=1;
     
     cout<<"Enter a number:"<<endl;
     cin>>a;
     cout<<"The fibonacci series of "<<a<<" numbers is:";
     cout<<f1<<" "<<f2;
-    for (i=3;i<=a;i++)
+    for (j=3;j<=a;j++)
     {
         f3=f1+f2;
         cout<<" "<<f3;
