620bdd48deb7dd7627a6b88cb09b57b41b782407
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-14 22:44:24 +0530

    67 - Given a number find a nearest greater multiple of 10

---
+++ set7/set7-7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,i;
+	cout<<"Enter a number:";
+	cin>>n;
+	for(i=10;i<100;i+=10)
+	{
+	    if(n<i)
+	    {
+	        cout<<i;
+	        break;
+	    }
+	}
+}
