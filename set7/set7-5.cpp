2aa9c9d2a232a9ad2fbca63140d8718dcf3e3751
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-14 20:05:42 +0530

    65 - Given a string print it without using semicolon in your program

---
+++ set7/set7-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+string s;
+
+if(cout<<"Enter The String:")
+{
+    
+}
+if(getline(cin,s))
+{
+    
+}
+if (cout <<s) 
+{
+    
+}
+}
