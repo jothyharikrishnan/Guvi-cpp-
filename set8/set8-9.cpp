08e5ed77f1778106cacdcf7abfc86ec7b71d6dca
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-19 02:03:37 +0530

    79 - Given 2 numbers find their product and check whether it is a perfect square

---
+++ set8/set8-9.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+int res;
+int n,m,s;
+cout<<"Enter 2 Num:";
+cin>>n>>m;
+res=n*m;
+s=sqrt(res);
+    if(s==sqrt(res))
+    cout<<"yes";
+    else
+    cout<<"No";
+    return 0;
+}
