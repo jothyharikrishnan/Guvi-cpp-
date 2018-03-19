cd46aa490f84c3495b19fcc4e38d790f68156f42
jothyharikrishnan <36563413+jothyharikrishnan@users.noreply.github.com>
2018-03-19 14:06:47 +0530

    74 - Given a decimal(only 1 decimal place) number round it off to nearest integer(greater) and print it.

---
+++ set8/set8-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    int i;
+    cout<<"Enter a decimal(only 1 decimal place) number:";
+    cin>>a;
+    i=a;
+    cout<<i+1;
+    return 0;
+}
